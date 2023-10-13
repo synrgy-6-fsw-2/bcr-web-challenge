08ab3a72b1a24014c653727be568d9f0e7f5b3cf
David Nasrulloh <davidkrb52@gmail.com>
2023-10-13 20:55:44 +0700

    feat: submit tugas David Nasrulloh

--- app.js
+++ app.js
@@ -78,3 +78,103 @@ const carInventory = [
     ],
   },
 ];
+
+function searchByManufactures(searhcItem) {
+  return carInventory.filter((car) => 
+    car.manufacture.toLowerCase().includes(searhcItem.toLowerCase())
+  )
+}
+
+function filterByAvailability(availability) {
+  if(availability === 'all'){
+    return carInventory;
+  }
+  return carInventory.filter((car) => car.available === (availability === 'available') )
+}
+
+function filterByYears(year) {
+  if(year === 'all'){
+    return carInventory;
+  }
+  return carInventory.filter((car) => car.year === parseInt(year))
+}
+
+function filterByManufacture(manufacture) {
+  if(manufacture === 'all'){
+    return carInventory;
+  }
+  return carInventory.filter((car) => car.manufacture.toLowerCase() === manufacture.toLowerCase())
+}
+
+function filterByTransmition(transmision) {
+  if(transmision === 'all'){
+    return carInventory;
+  }
+  return carInventory.filter((car)=> car.transmission.toLowerCase() === transmision.toLowerCase())
+}
+
+// Setup DOM
+const searchFormField = document.getElementById('form-search-car-manufacture')
+const searchInput = document.querySelector('input[name="manufacture"]')
+
+// filter DOM
+const availabilityFilter = document.getElementById('filterAvailability')
+const manufactureFilter = document.getElementById('filterManufacture')
+const transmisionFilter = document.getElementById('filterTransmission')
+const yearFilter = document.getElementById('filterYear')
+
+const tableBody = document.getElementById('table-body-list-cars')
+
+function renderCars(cars) {
+  tableBody.innerHTML = ''
+  cars.map((car)=>{
+    const row = document.createElement('tr')
+    row.innerHTML = `
+      <td>${car.plate}</td>
+      <td>${car.manufacture}</td>
+      <td>${car.model}</td>
+      <td>${car.type}</td>
+      <td>${car.transmission}</td>
+      <td>${car.year}</td>
+      <td>${car.available ? 'Available' : 'Rented'}</td>
+    `
+    tableBody.appendChild(row)
+  })
+}
+
+searchFormField.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const searchValue = searchInput.value;
+  const result = searchByManufactures(searchValue)
+  // console.log(result)
+  renderCars(result)
+  searchInput.value = '';
+})
+
+availabilityFilter.addEventListener('change', () => {
+  const availabilityValue = availabilityFilter.value;
+  // console.log(availabilityValue)
+  const result = filterByAvailability(availabilityValue)
+  renderCars(result)
+})
+
+yearFilter.addEventListener('change', () => {
+  const year = yearFilter.value;
+  const result = filterByYears(year)
+  renderCars(result)
+})
+
+manufactureFilter.addEventListener('change', () => {
+  const manufactureValue = manufactureFilter.value;
+  // console.log(manufactureValue)
+  const result = filterByManufacture(manufactureValue)
+  renderCars(result)
+})
+
+transmisionFilter.addEventListener('change', () => {
+  const transmisionValue = transmisionFilter.value;
+  const result = filterByTransmition(transmisionValue)
+  renderCars(result)
+})
+
+renderCars(carInventory)

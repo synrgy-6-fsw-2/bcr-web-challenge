124e710e65cfeca1f1c68050b6df6d6a4b1b84a9
Ahmd Hdyt <94432124+maddhdyt@users.noreply.github.com>
2023-10-13 21:44:28 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -78,3 +78,119 @@ const carInventory = [
     ],
   },
 ];
+
+// Fungsi untuk mencari mobil berdasarkan manufacture
+function filterByManufacture(manufacture) {
+  if (manufacture === 'all') {
+    return carInventory;
+  }
+  return carInventory.filter(car => car.manufacture === manufacture);
+}
+// Fungsi untuk mencari mobil berdasarkan transmission
+function filterByTransmission(transmission) {
+  if (transmission === 'all') {
+    return carInventory;
+  }
+  return carInventory.filter(car => car.transmission === transmission);
+}
+
+// Fungsi untuk mencari mobil berdasarkan tahun
+function filterByYear(year) {
+  if (year === 'All') {
+    return carInventory; // Kembalikan semua data jika "All" dipilih
+  }
+  return carInventory.filter(car => car.year === year);
+}
+
+// Fungsi untuk mencari mobil berdasarkan availability
+function filterByAvailability(availability) {
+  if (availability === 'All') {
+    return carInventory;
+  }
+  return carInventory.filter(car => car.available === (availability === 'available'));
+}
+
+// Fungsi untuk menampilkan data ke dalam tabel HTML
+function displayFilteredData(filteredData) {
+  const tableBody = document.getElementById('table-body-list-cars');
+  tableBody.innerHTML = '';
+  // Loop melalui data yang sudah difilter dan tambahkan ke dalam tabel
+  filteredData.forEach(car => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${car.plate}</td>
+      <td>${car.manufacture}</td>
+      <td>${car.model}</td>
+      <td>${car.type}</td>
+      <td>${car.transmission}</td>
+      <td>${car.year}</td>
+      <td>${car.available ? 'Available' : 'Rented'}</td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+
+// Event listener untuk search form
+const searchForm = document.getElementById('form-search-car-manufacture');
+searchForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const manufactureInput = document.querySelector('input[name="manufacture"]').value;
+  const filteredData = filterByManufacture(manufactureInput);
+  displayFilteredData(filteredData);
+});
+
+// Event listener for filter by availability
+const filterAvailabilitySelect = document.getElementById('filterAvailability');
+filterAvailabilitySelect.addEventListener('change', function () {
+  const selectedAvailability = filterAvailabilitySelect.value;
+
+  if (selectedAvailability === 'all') {
+    displayFilteredData(carInventory);
+  } else {
+    const filteredData = filterByAvailability(selectedAvailability);
+    displayFilteredData(filteredData);
+  }
+});
+
+// Event listener for filter by manufacture
+const filterManufactureSelect = document.getElementById('filterManufacture');
+filterManufactureSelect.addEventListener('change', function () {
+  const selectedManufacture = filterManufactureSelect.value;
+
+  if (selectedManufacture === 'all') {
+    displayFilteredData(carInventory);
+  } else {
+    const filteredData = filterByManufacture(selectedManufacture);
+    displayFilteredData(filteredData);
+  }
+});
+
+
+// Event listener for filter by transmission
+const filterTransmissionSelect = document.getElementById('filterTransmission');
+filterTransmissionSelect.addEventListener('change', function () {
+  const selectedTransmission = filterTransmissionSelect.value;
+  if (selectedTransmission === 'all') {
+    displayFilteredData(carInventory);
+  } else {
+    const filteredData = filterByTransmission(selectedTransmission);
+    displayFilteredData(filteredData);
+  }
+});
+
+
+// Event listener for filter by year
+const filterYearSelect = document.getElementById('filterYear');
+filterYearSelect.addEventListener('change', function () {
+  const selectedYear = filterYearSelect.value;
+  if (selectedYear === 'Open this select menu' || selectedYear === 'All') {
+    displayFilteredData(carInventory);
+  } else {
+    const filteredData = filterByYear(parseInt(selectedYear));
+    displayFilteredData(filteredData);
+  }
+});
+
+
+// Initialize
+displayFilteredData(carInventory);

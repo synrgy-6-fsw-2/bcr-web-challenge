67d5a57f2a075ce7c39663665885c20c8e5e09bc
faridz-ter <faridzbos123@gmail.com>
2023-10-13 21:34:41 +0700

    init: kuis data struktur

--- app.js
+++ app.js
@@ -78,3 +78,96 @@ const carInventory = [
     ],
   },
 ];
+const filterAvailability = document.getElementById('filterAvailability');
+const filterManufacture = document.getElementById('filterManufacture');
+const filterTransmission = document.getElementById('filterTransmission');
+const filterYear = document.getElementById('filterYear');
+const tableBody = document.getElementById('table-body-list-cars');
+const searchFormField = document.getElementById('form-search-car-manufacture')
+const searchInput = document.querySelector('input[name="manufacture"]')
+
+console.log(carInventory[0].manufacture);
+
+function getUniqueValues(cars, key) {
+  const uniqueValues = [];
+  cars.forEach((car) => {
+    if (!uniqueValues.includes(car[key])) {
+      uniqueValues.push(car[key]);
+    }
+  });
+  return uniqueValues;
+}
+
+function populateFilterOptions(filterElement, values) {
+  filterElement.innerHTML = '<option value="all">All</option>';
+
+  values.forEach((value) => {
+    filterElement.innerHTML += `<option value="${value}">${value}</option>`;
+  });
+}
+
+const filters = {
+  manufacture: filterManufacture,
+  transmission: filterTransmission,
+  year: filterYear,
+};
+
+for (const filterKey in filters) {
+  const uniqueValues = getUniqueValues(carInventory, filterKey);
+  populateFilterOptions(filters[filterKey], uniqueValues);
+}
+
+function searchCar(e) {
+  const valueSearch = searchInput.value.trim().toLowerCase();
+  const filteredCars = carInventory.filter((car) => {
+    return car.manufacture.toLowerCase().includes(valueSearch);
+  });
+  showListcar(filteredCars);
+  searchInput.value = '';
+  e.preventDefault()
+}
+
+function updateCarList() {
+  const availabilityFilter = filterAvailability.value;
+  const manufactureFilter = filterManufacture.value;
+  const transmissionFilter = filterTransmission.value;
+  const yearFilter = filterYear.value;
+
+  const filteredCars = carInventory.filter((car) => {
+    return (
+      (availabilityFilter === 'all' || car.available === (availabilityFilter === 'available')) &&
+      (manufactureFilter === 'all' || car.manufacture === manufactureFilter) &&
+      (transmissionFilter === 'all' || car.transmission === transmissionFilter) &&
+      (yearFilter === 'all' || car.year.toString() === yearFilter)
+    );
+  });
+
+  console.log(manufactureFilter)
+  showListcar(filteredCars);
+}
+
+function showListcar(car) {
+  tableBody.innerHTML = '';
+
+  for (let i = 0; i < car.length; i++) {
+    const data = document.createElement('tr');
+    data.innerHTML = `
+    <td>${car[i].plate}</td>
+      <td>${car[i].manufacture}</td>
+      <td>${car[i].model}</td>
+      <td>${car[i].type}</td>
+      <td>${car[i].transmission}</td>
+      <td>${car[i].year}</td>
+      <td>${car[i].available ? 'Available' : 'Rented'}</td>
+    `;
+    tableBody.appendChild(data);
+  }
+}
+filterAvailability.addEventListener('change', updateCarList);
+filterManufacture.addEventListener('change', updateCarList);
+filterTransmission.addEventListener('change', updateCarList);
+filterYear.addEventListener('change', updateCarList);
+searchFormField.addEventListener('submit', searchCar)
+
+// Inisialisasi tampilan awal
+updateCarList();

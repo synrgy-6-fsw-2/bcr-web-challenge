1dd5e46c7206d6a5f40e771392ce6b57bc374cdf
sahat-parulian <sahatparulian@upi.edu>
2023-10-13 21:32:56 +0700

    add: filter javascript

--- app.js
+++ app.js
@@ -78,3 +78,118 @@ const carInventory = [
     ],
   },
 ];
+
+
+function carsDisplay(data){
+  const tableData = document.getElementById('table-body-list-cars');
+  tableData.innerHTML = '';
+
+
+  data.forEach((car) => {
+    const row = document.createElement('tr');
+     row.innerHTML = `
+      <td>${car.plate}</td>
+      <td>${car.manufacture}</td>
+      <td>${car.model}</td>
+      <td>${car.type}</td>
+      <td>${car.transmission}</td>
+      <td>${car.year}</td>
+      <td>${car.available ? '<span style="background-color: green; color: #ffff; padding-left:5%; padding-right:5%; padding-top:2%; padding-bottom:2%">Available</span>' : '<span style="background-color: red; color: #ffff; padding-left:5%; padding-right:5%; padding-top:2%; padding-bottom:2%">Rented</span>'}</td>
+    `;
+
+    tableData.appendChild(row);
+  })
+}
+
+carsDisplay(carInventory);
+
+const formSearch = document.getElementById('form-search-car-manufacture');
+const inputSearch = formSearch.querySelector('input')
+
+formSearch.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  const nilaiSearch = inputSearch.value.toLowerCase();
+  const carsFiltered = carInventory.filter((car) =>
+   car.manufacture.toLowerCase().includes(nilaiSearch)
+  );
+ carsDisplay(carsFiltered);
+});
+
+
+//Availibity
+const availibityCars = document.getElementById('filterAvailability');
+
+availibityCars.addEventListener('change', function() {
+  const selectAvailibity = availibityCars.value;
+
+  const carsFilter = carInventory.filter(function(car){
+    if (selectAvailibity === 'all'){
+      return true;
+    } else if (selectAvailibity === "available"){
+      return car.available === true;
+    } else if (selectAvailibity === "rented") {
+      return car.available === false;
+    }
+  })
+
+  carsDisplay(carsFilter);
+})
+
+
+//Manufacture
+function filterByManufacture(manufacture) {
+  const filteredCars = carInventory.filter((car) =>
+    car.manufacture.toLowerCase() === manufacture.toLowerCase()
+  );
+  carsDisplay(filteredCars);
+}
+
+const filterManufacture = document.getElementById('filterManufacture');
+
+filterManufacture.addEventListener('change', function() {
+  const selectedManufacture = filterManufacture.options[filterManufacture.selectedIndex].text;
+
+  if (selectedManufacture === 'Open this select menu') {
+    carsDisplay(carInventory);
+  } else {
+    filterByManufacture(selectedManufacture);
+  }
+});
+
+
+//Transmission
+function filtersTransmissi(transmission) {
+  const  filteredMobil= carInventory.filter((car) =>
+    car.transmission.toLowerCase() === transmission.toLowerCase()
+  );
+  carsDisplay(filteredMobil);
+}
+const filterTransmi = document.getElementById('filterTransmission');
+filterTransmi.addEventListener('change', function() {
+  const pilihTransmi = filterTransmi.options[filterTransmi.selectedIndex].text;
+
+  if (pilihTransmi=== 'Open this select menu') {
+    carsDisplay(carInventory);
+  } else {
+    filtersTransmissi(pilihTransmi);
+  }
+});
+
+
+//Year
+// Fungsi untuk melakukan filter berdasarkan Year
+function filterTahun(year) {
+  const filteredMobil = carInventory.filter((car) => car.year === parseInt(year));
+  carsDisplay(filteredMobil);
+}
+
+const filtersTahun = document.getElementById('filterYear');
+filtersTahun.addEventListener('change', function() {
+  const pilihTahun = filtersTahun.options[filtersTahun.selectedIndex].text;
+
+  if (pilihTahun === 'Open this select menu') {
+    carsDisplay(carInventory);
+  } else {
+    filterTahun(pilihTahun);
+  }
+});
